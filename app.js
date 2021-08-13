08cee95914fcb1c9a7742b76eab19639309f6931
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2021-08-14 01:02:08 +0200

    Forms and review options

--- app.js
+++ app.js
@@ -28,7 +28,21 @@ Vue.component('product',
         {{variant.vaiantColor}}
       </div>
       <button v-on:click="addCart('add-to-cart')" :class="{disabledButton: !inStock}">Add Cart</button>
+      <div>
+      <h2>Reviews</h2>
+      <p v-if="!review.length">No Reviews Yet!</p>
+      <ul>
+      <li v-for="reviews in review">
+      <p>{{reviews.name}}</p>
+      <p>{{reviews.review}}</p>
+      <p>{{reviews.rating}}</p>
+      </li>
+      </ul>
+      </div>
     </div>
+
+    <product-review @product-review="productReview"></product-review>
+
   </div>
     `,
     data(){
@@ -37,6 +51,7 @@ Vue.component('product',
             brand : 'Indian',
             selectedVariant : 0,
             inventory: 11,
+            review:[],
             description: ['80% cotton','20% polyster','gender-neutral'],
             variants: [{
                 variantId : 2234,
@@ -59,6 +74,9 @@ Vue.component('product',
                 updateProperty(val){
                     this.selectedVariant = val;
                     
+                },
+                productReview(productReview){
+                    this.review.push(productReview)
                 }
              },
              computed:{
@@ -93,6 +111,59 @@ Vue.component('product-details',{
     template : `
     <p>Product-Origin : {{product_details.origin}}</p>
     `
+})
+
+Vue.component('product-review',{
+    template : `
+    <form class="review-form" @submit.prevent="onSubmit">
+    <p>
+      <label for="name">Name:</label>
+      <input id="name" v-model="name" placeholder="name">
+    </p>
+    
+    <p>
+      <label for="review">Review:</label>      
+      <textarea id="review" v-model="review"></textarea>
+    </p>
+    
+    <p>
+      <label for="rating">Rating:</label>
+      <select id="rating" v-model.number="rating">
+        <option>5</option>
+        <option>4</option>
+        <option>3</option>
+        <option>2</option>
+        <option>1</option>
+      </select>
+    </p>
+        
+    <p>
+      <input type="submit" value="Submit">  
+    </p>    
+  
+  </form>
+    `,
+    data(){
+        return{
+            name: null,
+            review: null,
+            rating: null
+        }
+    },
+    methods:{
+        onSubmit(){
+           let productReview = {
+            name: this.name,
+            review: this.review,
+            rating: this.rating
+            }
+            this.$emit('product-review',productReview)
+
+            this.name = null
+            this.rating = null
+            this.review = null
+        }
+    }
 })
 
 
